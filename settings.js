3636823d2f17f3ae4decb3c6ed0757fe541739c9
LakiYa <111090896+LakiYa404@users.noreply.github.com>
2022-08-23 21:51:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,26 +32,26 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.owner = ['94767353051'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['94768353051'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94768353051'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'LakiYa' // Your name ඔබේ නම 
+global.botnma = '© ꪶꪖᴋɪꪗꪖ ʙꪮᴛ.!°' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = 'ꪶꪖᴋɪꪗꪖ' //ownernama,ownername
+global.packname = 'LakiYa Pack' // Sticker package name 
+global.author = 'LakiYa' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
-// Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
-global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
+// Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති `*© ꪶꪖᴋɪꪗꪖ ʙꪮᴛ ꀤꪀᥴ.!°*` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
+global.alivelogo = `https://telegra.ph/file/6093a01339d35077bd45d.jpg`
 global.alive =`Hello i am alive now
 
 Download song type .yt 
-Thanks fro using...`
+Thanks For using...`
 // Welcome massage  ඔබට කැමති නම් මෙයට ඔබෙ welcome massage  එක යොදන්න 
 global.welcome = `
-🌀 Thanks Fro using sl zim bot
+🌀 Thanks Fro using LakiYa MD BOT
 ✾ Some bugs fixing
 😼♥️
 `
@@ -60,7 +60,7 @@ global.sendwelcome = `add`
 // Good bye එක ඔනි නම් remove කියලා දාන්න
 global.sendgoodbye = `remove`
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `© ꪶꪖᴋɪꪗꪖ ʙꪮᴛ ꀤꪀᥴ.!°`
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
